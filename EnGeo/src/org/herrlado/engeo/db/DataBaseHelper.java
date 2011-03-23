26d2a7f27c90423b8f2eb514b70a82d35882756e
zkumsiashvili <zkumsiashvili@e82fc9c8-0917-7146-5ab2-b93fe495bc3d>
2011-03-23 23:29:27 +0000

    ka strings added

--- EnGeo/src/org/herrlado/engeo/db/DataBaseHelper.java
+++ EnGeo/src/org/herrlado/engeo/db/DataBaseHelper.java
@@ -4,14 +4,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.zip.ZipInputStream;
-
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Environment;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
