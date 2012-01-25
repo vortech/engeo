f182b835649ee6bc77c56cc5254aeac89dff0ba7
zkumsiashvili@gmail.com <zkumsiashvili@gmail.com@e82fc9c8-0917-7146-5ab2-b93fe495bc3d>
2012-01-25 12:31:17 +0000

    added translate.ge support

--- trunk/EnGeo/src/org/herrlado/engeo/WordlistAdapter.java
+++ trunk/EnGeo/src/org/herrlado/engeo/WordlistAdapter.java
@@ -27,7 +27,11 @@ public class WordlistAdapter extends ResourceCursorAdapter implements
 	private static final String SQL_GEO = "SELECT t2.id as _id, t1.eng as translate, t1.transcription, t2.geo as original, t4.name, t4.abbr FROM eng t1, geo t2, geo_eng t3, types t4 WHERE t2.geo >= ? AND t3.eng_id=t1.id AND t2.id=t3.geo_id AND t4.id=t2.type ORDER BY t2.geo LIMIT 25";
 
 	private static final String TAG = Wordlist.class.getSimpleName();
-
+	
+	private static final String geo = "http://translate.ge/g.aspx?w=";
+	
+	private static final String eng = "http://translate.ge/q.aspx?w=";
+	
 	DataBaseHelper db;
 
 	private static boolean isGeo(CharSequence w) {
@@ -56,28 +60,31 @@ public class WordlistAdapter extends ResourceCursorAdapter implements
 
 	}
 
-	public final HttpGet ENG_GET = new HttpGet("http://translate.ge/q.aspx?w=");
+	public final HttpGet ENG_GET = new HttpGet(eng);
 
-	public final HttpGet GEO_GET = new HttpGet("http://translate.ge/g.aspx?w=");
+	public final HttpGet GEO_GET = new HttpGet(geo);
 
 	DefaultHttpClient client = new DefaultHttpClient();
 
-	public void loadTranslateGe(CharSequence w, ArrayList<String> results) {
+	public String loadTranslateGe(CharSequence w) {
 
-		HttpGet get;
+		String url;
 		if (isGeo(w)) {
-			get = GEO_GET;
+			url = geo + w;
 		} else {
-			get = ENG_GET;
+			url = eng + w;
 		}
-		get.getParams().setParameter("w", w);
+
+		HttpGet get = new HttpGet(url);
+		//get.getParams().setParameter("w", w);
+		
 		HttpResponse response = null;
 		try {
 			response = client.execute(get);
 
 			if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
 				Log.w("EnGEO", response.getStatusLine().toString());
-				return;
+				return "";
 			}
 
 			String content = Utils
@@ -85,20 +92,12 @@ public class WordlistAdapter extends ResourceCursorAdapter implements
 
 			LineNumberReader reader = new LineNumberReader(new StringReader(
 					content));
-			String line;
-			// StringBuilder sb = new StringBuilder();
-			while ((line = reader.readLine()) != null) {
-				int idx = line.indexOf("-");
-				if (idx == -1)
-					continue;
-				String term = line.substring(0, idx).trim();
-				String desc = line.substring(idx, 20).trim();
-				results.add(term + " = " + desc + "...");
-			}
-
+			String line = reader.readLine();
+			
+			return line;
 		} catch (Exception e) {
 			Log.w(TAG, "Error on client.execute", e);
-			return;
+			return "";
 		}
 
 	}
@@ -123,36 +122,17 @@ public class WordlistAdapter extends ResourceCursorAdapter implements
 		final String SQL;
 		final String[] args;
 
-		boolean engeo = true;
-
 		if (isGeo(s)) {
 			SQL = SQL_GEO;
-			// args = new String[] { to_latin(s.toString().trim()) + "%"};
 			args = new String[] { s.toString().trim() + "%" };
-			engeo = false;
 		} else {
 			SQL = SQL_ENG;
 			args = new String[] { s.toString().toLowerCase().trim() + "%" };
 		}
 
-		// new AsyncTask<Void, Void, Void>(){
-		//
-		// @Override
-		// protected Void doInBackground(Void... params) {
 		Cursor c = db.myDataBase.rawQuery(SQL, args);
 		changeCursor(c);
-		// return null;
-		// }
-		//
-		// }.execute();
-		/*
-		 * ArrayList<String> results = new ArrayList<String>(); if (c != null) {
-		 * if (c.moveToFirst()) { do { if (engeo) {
-		 * 
-		 * } else {
-		 * 
-		 * } } while (c.moveToNext()); } }
-		 */
+		
 	}
 
 	@Override
