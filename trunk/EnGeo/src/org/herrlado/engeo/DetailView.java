d5ee59eb52dbd19834c09bca608d2df1b8a08ac3
zkumsiashvili@gmail.com <zkumsiashvili@gmail.com@e82fc9c8-0917-7146-5ab2-b93fe495bc3d>
2012-01-30 13:15:28 +0000

    progress dialog  and search bar with round corners added

--- trunk/EnGeo/src/org/herrlado/engeo/DetailView.java
+++ trunk/EnGeo/src/org/herrlado/engeo/DetailView.java
@@ -1,16 +1,21 @@
 package org.herrlado.engeo;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class DetailView extends Activity {
 
 	private static final String geo = "http://translate.ge/g.aspx?w=";
 	private static final String eng = "http://translate.ge/q.aspx?w=";
+	WebView wbView;
+	TextView tv;
+	ProgressDialog dialog;
 	CharSequence[] extras;
 	String url;
 
@@ -20,7 +25,7 @@ public class DetailView extends Activity {
 		getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.detail);
 
-		TextView tv = (TextView) findViewById(R.id.detail);
+		tv = (TextView) findViewById(R.id.detail);
 		Intent sender = getIntent();
 		extras = sender.getExtras().getCharSequenceArray("extras");
 
@@ -35,8 +40,23 @@ public class DetailView extends Activity {
 			str += extras[i] + "\n";
 		}
 		tv.setText(str);
+		
+		dialog = ProgressDialog.show(this, "Loading", "Loading data from translate.ge...");
+		wbView = (WebView) findViewById(R.id.translatege);
+		
+		wbView.setWebViewClient(new WebViewClient(){
+			public boolean shouldOverrideUrlLoading(WebView view, String url){
+				view.loadUrl(url);
+				return true;
+			}
+			
+			public void onPageFinished(WebView view, String url) {
+				if(dialog.isShowing()){
+					dialog.dismiss();
+				}
+			}
 
-		WebView wbView = (WebView) findViewById(R.id.translatege);
+		});
 		wbView.loadUrl(url);
 
 	}
