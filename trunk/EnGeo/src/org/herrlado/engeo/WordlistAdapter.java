8870257e9e2e12bd867d565cd2181d7de3934b78
zkumsiashvili@gmail.com <zkumsiashvili@gmail.com@e82fc9c8-0917-7146-5ab2-b93fe495bc3d>
2012-02-03 22:07:54 +0000

    textview font color changed

--- trunk/EnGeo/src/org/herrlado/engeo/WordlistAdapter.java
+++ trunk/EnGeo/src/org/herrlado/engeo/WordlistAdapter.java
@@ -13,28 +13,29 @@ import android.widget.TextView;
 public class WordlistAdapter extends ResourceCursorAdapter implements
 		TextWatcher {
 
-	private static final String SQL_ENG = "SELECT MAX(t1.id) as _id, t1.eng as original, t1.transcription, group_concat(t2.geo, ', ') as translate, t4.name, t4.abbr FROM eng t1, geo t2, geo_eng t3, types t4 WHERE t1.eng >= ? AND t3.eng_id=t1.id AND t2.id=t3.geo_id AND t4.id=t2.type GROUP BY t1.eng LIMIT 25";
+	private static final String SQL_ENG = "SELECT t1.id as _id, t1.eng as original, t1.transcription, group_concat(t2.geo, ', ') as translate, t4.name, t4.abbr FROM eng t1, geo t2, geo_eng t3, types t4 WHERE t1.eng >= ? AND t3.eng_id=t1.id AND t2.id=t3.geo_id AND t4.id=t1.type GROUP BY t1.eng LIMIT 25";
 
-	private static final String SQL_GEO = "SELECT MAX(t2.id) as _id, group_concat(t1.eng, ', ') as translate, t1.transcription, t2.geo as original, t4.name, t4.abbr FROM eng t1, geo t2, geo_eng t3, types t4 WHERE t2.geo >= ? AND t3.eng_id=t1.id AND t2.id=t3.geo_id AND t4.id=t2.type GROUP BY t2.geo LIMIT 25";
+	private static final String SQL_GEO = "SELECT t2.id as _id, group_concat(t1.eng, ', ') as translate, t1.transcription, t2.geo as original, t4.name, t4.abbr FROM eng t1, geo t2, geo_eng t3, types t4 WHERE t2.geo >= ? AND t3.eng_id=t1.id AND t2.id=t3.geo_id AND t4.id=t2.type GROUP BY t2.geo LIMIT 25";
 
 	private static final String TAG = Wordlist.class.getSimpleName();
 
 	DataBaseHelper db;
 
 	public WordlistAdapter(Wordlist wordlist) {
-		super(wordlist, android.R.layout.simple_list_item_2, null, true);
+		super(wordlist, R.layout.fontcolor, null, true);
 		db = new DataBaseHelper(wordlist);
 		db.openDataBase();
 	}
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		final TextView orig = (TextView) view.findViewById(android.R.id.text1);
+		final TextView orig = (TextView) view.findViewById(R.id.word_original);
 
 		String original = cursor.getString(cursor.getColumnIndex("original"));
 		orig.setText(original);
 
-		final TextView trans = (TextView) view.findViewById(android.R.id.text2);
+		final TextView trans = (TextView) view
+				.findViewById(R.id.word_translate);
 
 		String tra = cursor.getString(cursor.getColumnIndex("translate"));
 		trans.setText(tra);
