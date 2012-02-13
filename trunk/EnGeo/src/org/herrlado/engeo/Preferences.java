7e1212f88a7b82aa80d1940e17514aa18f1099dd
zkumsiashvili@gmail.com <zkumsiashvili@gmail.com@e82fc9c8-0917-7146-5ab2-b93fe495bc3d>
2012-02-13 20:27:05 +0000

    preferences edited

--- trunk/EnGeo/src/org/herrlado/engeo/Preferences.java
+++ trunk/EnGeo/src/org/herrlado/engeo/Preferences.java
@@ -1,10 +1,6 @@
 package org.herrlado.engeo;
 
-import java.util.Locale;
-
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -18,16 +14,6 @@ import de.ub0r.android.lib.Log;
  */
 public class Preferences extends PreferenceActivity implements
 		SharedPreferences.OnSharedPreferenceChangeListener {
-
-	public static final String CHANGE_LANG = "change_lang";
-
-	public static final String LANG_KA = "ქართული";
-
-	/** Preference's name: theme. */
-	private static final String PREFS_THEME = "themes";
-
-	/** Theme: light. */
-	private static final String THEME_LIGHT = "Light";
 
 	/** Tag for output. */
 	public static final String TAG = "engeo.pref";
@@ -52,56 +38,11 @@ public class Preferences extends PreferenceActivity implements
 		}
 	}
 
-	public final void localeChange() {
-		this.addPreferencesFromResource(R.xml.prefs);
-		SharedPreferences sharedPref = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		Preference p = this.findPreference(sharedPref.getString(CHANGE_LANG,
-				LANG_KA));
-		if (p != null && LANG_KA.equals(p)) {
-			p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-
-				@Override
-				public boolean onPreferenceClick(Preference preference) {
-					// TODO Auto-generated method stub
-					Locale local = new Locale("ka");
-					Locale.setDefault(local);
-					Configuration config = new Configuration();
-					config.locale = local;
-					getBaseContext().getResources()
-							.updateConfiguration(
-									config,
-									getBaseContext().getResources()
-											.getDisplayMetrics());
-
-					return true;
-				}
-			});
-		}
-	}
-
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
 		final SharedPreferences p = PreferenceManager
 				.getDefaultSharedPreferences(// .
 				Preferences.this);
-	}
-
-	/**
-	 * Get Theme from Preferences.
-	 * 
-	 * @param context
-	 *            {@link Context}
-	 * @return theme
-	 */
-	static final int getTheme(final Context context) {
-		final SharedPreferences p = PreferenceManager
-				.getDefaultSharedPreferences(context);
-		final String s = p.getString(PREFS_THEME, THEME_LIGHT);
-		if (s != null && THEME_LIGHT.equals(s)) {
-			return android.R.style.Theme_Light;
-		}
-		return android.R.style.Theme_Black;
 	}
 }
